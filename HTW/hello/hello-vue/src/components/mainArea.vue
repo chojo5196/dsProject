a3d00d20dc0fa3b1ea55c830f5d2644b02ca7986
JungGyeongmi <gm950715@gmail.com>
2022-01-01 21:21:51 +0900

    temp

---
+++ HTW/hello/hello-vue/src/components/mainArea.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'topNav',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
---
+++ HTW/hello/hello-vue/src/components/topNav.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="top-nav">
+    <h1>{{ msg }}</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'topNav',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+  .top-nav {
+    color: red;   
+  }
+</style>
