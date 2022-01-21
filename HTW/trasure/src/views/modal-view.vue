e617efb4cbefe7eba5c92ea5b22242efda4a3bc4
seongjin <seongjin2427@gmail.com>
2022-01-22 00:37:33 +0900

    add transferBus

---
+++ HTW/trasure/src/views/modal-view.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <div>
+          {{ sample.author }}
+      </div>
+  </div>
+</template>
+
+
+<script>
+import eventBus from "../assets/eventBus"
+export default {
+    name: 'modalView',
+    data() {
+        return {
+            sample: "aa"
+        }
+    },
+    created: function() {
+        eventBus.$on('transferBus', function(node) {
+            console.log("이벤트 수신");
+            console.log(node);
+            
+            this.sample = node;
+            
+            console.log(this.sample.author)
+        });
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
